ba5f3508f49dbba1af27ab07a2d75df22e78a350
KhanhNguyen <nguyentrungk461@gmail.com>
2024-10-22 15:38:37 +0700

    init project

---
+++ src/main/java/com/kit/sonahotel_booking/SonaHotelBookingApplication.java
@@ -0,0 +1,13 @@
+package com.kit.sonahotel_booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonaHotelBookingApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SonaHotelBookingApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/kit/sonahotel_booking/SonaHotelBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kit.sonahotel_booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonaHotelBookingApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
